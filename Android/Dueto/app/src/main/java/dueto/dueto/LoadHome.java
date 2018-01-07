5c8f4875130adcf2757abbec05b3a6fae9adaf38
Ben <benji21997@gmail.com>
2018-01-07 17:35:49 -0500

    Added a new class to load videos Edited the main activity xml to match the proposed layout

---
+++ Android/Dueto/app/src/main/java/dueto/dueto/LoadHome.java
@@ -0,0 +1,28 @@
+package dueto.dueto;
+
+/**
+ * Created by ben on 07/01/18.
+ * This class loads the videos, which should be displayed on the "Home" and places them in table rows in the layout.
+ * Methods have to be defined as static. If the server cannot be reached this class will provide the private method
+ * notAvailable, which will display a connection error message.
+ */
+
+public class LoadHome
+{
+    /**
+     *
+     */
+    public static void generateVideos(/*User user*/)
+    {
+
+    }
+
+
+    /**
+    * notAvailable()
+     */
+    private static void notAvailable()
+    {
+
+    }
+}
