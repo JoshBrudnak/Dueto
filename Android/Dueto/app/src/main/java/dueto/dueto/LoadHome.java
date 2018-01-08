292f0934dafb3e7036a1e6b82df7ecde8c56a3af
Ben <benji21997@gmail.com>
2018-01-07 23:22:04 -0500

    Experimenting with video loading Changed MainActivity parent

--- Android/Dueto/app/src/main/java/dueto/dueto/LoadHome.java
+++ Android/Dueto/app/src/main/java/dueto/dueto/LoadHome.java
@@ -1,19 +1,40 @@
 package dueto.dueto;
 
+import android.app.Activity;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.VideoView;
+import java.util.ArrayList;
+
 /**
  * Created by ben on 07/01/18.
  * This class loads the videos, which should be displayed on the "Home" and places them in table rows in the layout.
- * Methods have to be defined as static. If the server cannot be reached this class will provide the private method
+ * If the server cannot be reached this class will provide the private method
  * notAvailable, which will display a connection error message.
  */
 
-public class LoadHome
+public class LoadHome extends Activity
 {
     /**
      *
      */
-    public static void generateVideos(/*User user*/)
+    public void generateVideos(/*User user*/)
     {
+        ArrayList<ImageView> viewers = new ArrayList<>();
+        TableLayout table = (TableLayout)findViewById(R.id.homeTable);
+        TableRow row = new TableRow(null);
+
+
+        //Load videos into viewers
+
+
+        for(ImageView v : viewers)
+        {
+            row.addView(v);
+            table.addView(row);
+        }
+
 
     }
 
