57ebef694d1eab2ab3e0462dd761d3d9b0deb961
Josh31415 <jobrud314@gmail.com>
2018-01-14 07:26:37 -0700

    Reverted last commit :(

---
+++ iOS /Dueto/Dueto/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Dueto
+//
+//  Created by Laith Rafidi on 1/7/18.
+//  Copyright © 2018 Dueto. All rights reserved.
+//
+
+import UIKit
+
+struct cellData {
+    let cell : Int!
+    let text : String!
+    let image : UIImage!
+    
+}
+
+class TableViewController: UITableViewController {
+    
+    var arrayOfCellData = [cellData]()
+    
+    override func viewDidLoad() {
+        arrayOfCellData = [cellData(cell : 1, text : "", image : #imageLiteral(resourceName: "piano")),
+        cellData(cell : 1, text : "", image : #imageLiteral(resourceName: "piano")), cellData(cell : 1, text : "", image : #imageLiteral(resourceName: "piano"))]
+        
+                
+
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfCellData.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+            let cell = Bundle.main.loadNibNamed("TableViewCell1", owner: self, options: nil)?.first as! TableViewCell1
+            cell.mainImageView.image = arrayOfCellData[indexPath.row].image
+            cell.mainLabel.text = arrayOfCellData[indexPath.row].text
+            
+            return cell
+        
+    
+    }
+
+
+}
+
---
+++ react/dueto/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Dueto from './Dueto';
+import registerServiceWorker from './registerServiceWorker';
+import { MuiThemeProvider, createMuiTheme } from "material-ui/styles"
+
+ReactDOM.render(
+  <MuiThemeProvider>
+    <Dueto/>
+  </MuiThemeProvider>
+  , document.getElementById('root'));
+
+registerServiceWorker();
