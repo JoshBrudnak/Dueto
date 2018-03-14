ddc3dc9f7d8c8fc10cf444c0fc1b862d7d8423cd
Sam <35157225+tahiris719@users.noreply.github.com>
2018-03-14 12:26:43 -0400

    Profile Char. Limit
    10 character limit to profile texts

--- Android/Dueto/app/src/main/java/dueto/dueto/PostsFragment.java
+++ Android/Dueto/app/src/main/java/dueto/dueto/PostsFragment.java
@@ -1,23 +1,44 @@
 package dueto.dueto;
 
+import android.app.ListFragment;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+import static android.content.ContentValues.TAG;
 
 
 public class PostsFragment extends Fragment {
 
     private boolean scrollable = true;
 
+
+//    @Override
+//    public void onCreateView(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+//
+//    @Override
+//    public void onActivityCreated(Bundle savedInstanceState){
+//        super.onActivityCreated(savedInstanceState);
+//        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.))
+//    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_posts, container, false);
     }
+
+
 
 }
